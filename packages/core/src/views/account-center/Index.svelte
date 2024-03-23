d6df58705db3056206cac9a9098ac64371ededab
SeamusAbbott <lbMJZeEv27304@outlook.com>
2024-03-23 20:01:51 +0800

    rem

---
+++ packages/core/src/views/account-center/Index.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { onDestroy } from 'svelte'
+  import { updateAccountCenter } from '../../store/actions.js'
+  import { wallets$ } from '../../streams.js'
+  import { state } from '../../store/index.js'
+  import { shareReplay, startWith } from 'rxjs/operators'
+  import Maximized from './Maximized.svelte'
+  import Minimized from './Minimized.svelte'
+  import Micro from './Micro.svelte'
+
+  export let mountInContainer = false
+
+  const accountCenter$ = state
+    .select('accountCenter')
+    .pipe(startWith(state.get().accountCenter), shareReplay(1))
+
+  onDestroy(minimize)
+
+  function minimize() {
+    if ($accountCenter$.expanded) {
+      updateAccountCenter({ expanded: false })
+    }
+  }
+</script>
+
+<svelte:window on:click={minimize} />
+
+{#if mountInContainer}
+  {#if $wallets$.length}
+    <div class="container flex flex-column fixed z-indexed">
+      <svelte:self />
+    </div>
+  {/if}
+{:else if !$accountCenter$.expanded && !$accountCenter$.minimal}
+  <!-- minimized -->
+  <Minimized />
+{:else if !$accountCenter$.expanded && $accountCenter$.minimal}
+  <!-- micro -->
+  <Micro />
+{:else}
+  <!-- maximized -->
+  <Maximized />
+{/if}
---
+++ packages/walletconnect/src/index.ts
@@ -0,0 +1,73 @@
+import type { WalletInit } from '@web3-onboard/common'
+import type { Web3ModalConfig } from '@web3modal/standalone'
+import v1 from './v1.js'
+import v2 from './v2.js'
+
+export type WalletConnectOptions = {
+  /**
+   * Optional function to handle WalletConnect URI when it becomes available
+   */
+  handleUri?: (uri: string) => Promise<unknown>
+  connectFirstChainId?: boolean
+  bridge?: string
+  qrcodeModalOptions?: {
+    mobileLinks: string[]
+  }
+} & (
+  | {
+      /**
+       * Defaults to version: 1 - this behavior will be deprecated after the WalletConnect v1 sunset
+       */
+      version?: 1
+    }
+  | {
+      /**
+       * Project ID associated with [WalletConnect account](https://cloud.walletconnect.com)
+       */
+      projectId: string
+      /**
+       * Defaults to version: 1 - this behavior will be deprecated after the WalletConnect v1 sunset
+       */
+      version: 2
+      /**
+       * List of Required Chain(s) ID for wallets to support in number format (integer or hex)
+       * Defaults to [1] - Ethereum
+       * The chains defined within the web3-onboard config will define the
+       * optional chains for the WalletConnect module
+       */
+      requiredChains?: number[] | undefined
+      /**
+       * `undefined` by default, see https://docs.walletconnect.com/2.0/web3modal/options
+       */
+      qrModalOptions?: Pick<
+        Web3ModalConfig,
+        | 'themeMode'
+        | 'themeVariables'
+        | 'chainImages'
+        | 'desktopWallets'
+        | 'enableExplorer'
+        | 'explorerRecommendedWalletIds'
+        | 'explorerExcludedWalletIds'
+        | 'mobileWallets'
+        | 'privacyPolicyUrl'
+        | 'termsOfServiceUrl'
+        | 'tokenImages'
+        | 'walletImages'
+      >
+    }
+)
+
+export const isHexString = (value: string | number) => {
+  if (typeof value !== 'string' || !value.match(/^0x[0-9A-Fa-f]*$/)) {
+    return false
+  }
+
+  return true
+}
+
+function walletConnect(options?: WalletConnectOptions): WalletInit {
+  const version = (options && options.version) || 1
+  return version === 1 ? v1(options) : v2(options)
+}
+
+export default walletConnect
